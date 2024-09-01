3a399f6dc72af1126b653154ab373ccd473c1624
Chris Flinn <christopher.flinn@gmail.com>
2024-09-02 01:27:14 +1000

    A

---
+++ 970/sakurako_exam.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+auto main() -> int {
+    auto t = int{};
+    std::cin >> t;
+
+    while (t--) {
+        auto a = int{};
+        auto b = int{};
+
+        std::cin >> a >> b;
+
+        auto sum  = a + 2 * b;
+
+        if (sum % 2 == 0 && (a >= (sum / 2) % 2)) {
+            std::cout << "Yes" << std::endl;
+        } else {
+            std::cout << "No" << std::endl;
+        }
+    }
+}
