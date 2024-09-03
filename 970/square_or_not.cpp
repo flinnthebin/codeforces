b0fa6ceed540f91096880b748aa0245446fe95d5
Chris Flinn <christopher.flinn@gmail.com>
2024-09-04 02:51:39 +1000

    update

---
+++ 970/square_or_not.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+auto main() -> int {
+    auto t = int{};
+    std::cin >> t;
+
+    while (t--) {
+        auto n = int{};
+        auto s = std::string{};
+
+        std::cin >> n >> s;
+
+        if (s[n / 2] == '0') {
+            std::cout << "Yes" << std::endl;
+        } else {
+            std::cout << "No" << std::endl;
+        }
+    }
+}
