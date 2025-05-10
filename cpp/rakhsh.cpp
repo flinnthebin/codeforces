4c7565c0c8a7965cf7c039d42cbb154236c70f10
chris flinn <christopher.flinn@gmail.com>
2025-05-10 10:13:51 +1000

    update

---
+++ cpp/rakhsh.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+auto main() -> int {
+  std::ios::sync_with_stdio(0);
+  std::cin.tie(0);
+
+  auto t = int{}; // number of test cases
+  auto n = int{}; // length of binary string s
+  auto m = int{}; // maximum number of consecutive 0s
+  auto k = int{}; // size of interval for bitflip
+  auto s = std::string{}; // binary string
+
+  std::cin >> t;
+
+  for (auto x = 0; x < t; ++x) {
+    std::cin >> n >> m >> k >> s;
+    assert(n = s.leng
+  }
+}
