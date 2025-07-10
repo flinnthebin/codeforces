378b78c9aa61ab98fd62f7147075695b62f27855
chris flinn <christopher.flinn@gmail.com>
2025-07-11 01:14:09 +1000

    update

--- dev/queue_at_school/queue_at_school.cpp
+++ dev/queue_at_school/queue_at_school.cpp
@@ -1,1 +1,20 @@
 #include <bits/stdc++.h>
+
+auto main() -> int {
+  int n; int t; std::string q;
+  std::cin >> n >> t >> q;
+  std::vector<char> queue(q.begin(), q.end());
+
+  for (int _ = 0; _ < t; ++_) {
+    int x = 0;
+    while (x < n - 1) {
+      if (queue[x] == 'B' && queue[x+1] == 'G') {
+        std::iter_swap(queue.begin() + x, queue.begin() + x + 1);
+        x++;
+      }
+      x++;
+    }
+  }
+  std::string result = std::string(queue.begin(), queue.end());
+  std::cout << result << "\n";
+  }
