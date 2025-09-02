53d4b86a555c133b5dd4a8b6297a72c258074a1e
Chris Flinn <christopher.flinn@gmail.com>
2025-09-02 18:59:34 +1000

    732A Buy A Shovel

---
+++ rust/src/bin/buy-a-shovel.rs
@@ -0,0 +1,26 @@
+use std::io::{self, Read};
+
+struct Scanner<'a> {
+    it: std::str::SplitAsciiWhitespace<'a>,
+}
+
+impl<'a> Scanner<'a> {
+    fn new(s: &'a str) -> Self {
+        Self {
+            it: s.split_ascii_whitespace(),
+        }
+    }
+
+    fn next<T: std::str::FromStr>(&mut self) -> T {
+        self.it.next().unwrap().parse().ok().unwrap()
+    }
+}
+
+fn shovel() {}
+
+fn main() {
+    let mut buf = String::new();
+    io::stdin().read_to_string(&mut buf).unwrap();
+    let mut sc = Scanner::new(&buf);
+    let t: usize = sc.next();
+}
